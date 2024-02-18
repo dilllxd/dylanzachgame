42cd23282e0a4d26c0e22199391312f976de1a13
Dylan <dylan@dylanh.dev>
2024-02-17 23:46:58 -0500

    fix(debug): forgot some logging
    added logging for player anim

--- OurGame/characters/happy_boo/happy_boo.gd
+++ OurGame/characters/happy_boo/happy_boo.gd
@@ -3,7 +3,9 @@ extends Node2D
 
 func play_idle_animation():
 	%AnimationPlayer.play("idle")
+	print("happyboo script play idle")
 
 
 func play_walk_animation():
 	%AnimationPlayer.play("walk")
+	print("happyboo script play walk")
