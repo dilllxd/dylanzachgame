2567633e74ee6115b4f8d3d00f86d66cb0ccb00c
Dylan <dylan@dylanh.dev>
2024-02-17 23:41:36 -0500

    feat(debug): adding debugging to animations
    this should hopefully let us see what animation is played and then when the lag happens

--- OurGame/characters/slime/slime.gd
+++ OurGame/characters/slime/slime.gd
@@ -3,8 +3,11 @@ extends Node2D
 
 func play_walk():
 	%AnimationPlayer.play("walk")
+	print("slime script play walk")
 
 
 func play_hurt():
 	%AnimationPlayer.play("hurt")
+	print("slime script play hurt")
 	%AnimationPlayer.queue("walk")
+	print("slime script play walk after hurt")
